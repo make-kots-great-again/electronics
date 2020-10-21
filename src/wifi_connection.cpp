d6fc84fe9ba0dc3b4f9de46dabdcdc5e09d0e2ed
MMichotte <martin.michotte@gmail.com>
2020-10-21 22:36:04 +0200

    added button action on wifi-con

--- src/wifi_connection.cpp
+++ src/wifi_connection.cpp
@@ -11,22 +11,55 @@ void connectToWifi()
     
     int netIndex = 0;
     encValue enc;
-    while(!checkBtnPress().wasPressed){
-        enc = getEncoderValue();
-        if(enc.hasChanged){
-            Serial.println(enc.direction);
-            if(enc.direction == 1){
-                netIndex = (netIndex < networks.nets-1)?  netIndex + 1 : netIndex;
-                lcdClearLine(1);
-                lcdPrint(networks.SSID[netIndex]);
-            }
-            else if(enc.direction == -1){
-                netIndex = (netIndex > 0)?  netIndex - 1 : netIndex;
-                lcdClearLine(1);
-                lcdPrint(networks.SSID[netIndex]);
+    while(true){
+        if(!checkBtnPress().wasPressed){
+            //TODO a verifier une fois composants souder!
+            enc = getEncoderValue();
+            if(enc.hasChanged){
+                Serial.println(enc.direction);
+                if(enc.direction == 1){
+                    netIndex = (netIndex < networks.nets-1)?  netIndex + 1 : netIndex;
+                    lcdClearLine(1);
+                    lcdPrint(networks.SSID[netIndex]);
+                }
+                else if(enc.direction == -1){
+                    netIndex = (netIndex > 0)?  netIndex - 1 : netIndex;
+                    lcdClearLine(1);
+                    lcdPrint(networks.SSID[netIndex]);
+                }
             }
         }
-    }
-    //TODO a verifier une fois composants souder!
+        else{
+            if(checkBtnPress().time < 1000){
+                if(connectToNetwork(networks.SSID[netIndex],getPassword())){
+                    //Connection réussie
+                    lcdClear();
+                    lcdPrint("Conn. réussie!");
+                    delay(2500);
+                    lcdClear();
+                    break;
+                }
+                else{
+                    //Connection échouée
+                    lcdClear();
+                    lcdPrint("Conn. échouée!");
+                    delay(2500);
+                    lcdClear();
+                    connectToWifi();
+                    break;
+                }
+            }
+            else{
+                //l'utilisateur souhaite rafraîchir la liste des PA.
+                networks = listNetworks();
+            }
+        }
 
+    }
+}
+
+
+String getPassword()
+{
+    //TODO
 }
