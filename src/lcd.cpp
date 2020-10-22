d15073fd698150724fbb293f8687c4f4c54269ca
MMichotte <martin.michotte@gmail.com>
2020-10-22 11:13:14 +0200

    changed lcd input for colabs

--- src/lcd.cpp
+++ src/lcd.cpp
@@ -1,11 +1,11 @@
 #include "lcd.h"
 
-//LiquidCrystal_I2C lcd(0x27, 16, 2);
-LiquidCrystal_I2C_GY lcd(0x38,16,2); //! Uniquement pour LCD de martin (pas le meme chipset)
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+//LiquidCrystal_I2C_GY lcd(0x38,16,2); //! Uniquement pour LCD de martin (pas le meme chipset)
 
 void lcdSetup() {
-    //lcd.begin();
-    lcd.init(); //! Uniquement pour LCD de martin (pas le meme chipset)
+    lcd.begin();
+    //lcd.init(); //! Uniquement pour LCD de martin (pas le meme chipset)
     lcd.backlight();       
     lcd.noAutoscroll();
     lcd.setCursor(0, 0); 
