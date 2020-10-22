f332866f2f26de788b93671818e2d724ed396277
MMichotte <martin.michotte@gmail.com>
2020-10-22 11:12:21 +0200

    wifi-con updt

--- src/wifi_connection.cpp
+++ src/wifi_connection.cpp
@@ -4,34 +4,42 @@ void connectToWifi()
 {
     lcdPrint("Recherche de PA");
     networks networks = listNetworks();
-    lcdClear(); 
+    lcdClear();
     lcdPrint("Choose Network:");
-    lcdCursor(1,0); 
+    lcdCursor(1, 0);
     lcdPrint(networks.SSID[0]);
-    
+
     int netIndex = 0;
     encValue enc;
-    while(true){
-        if(!checkBtnPress().wasPressed){
+    while (true)
+    {
+        if (!checkBtnPress().wasPressed)
+        {
             //TODO a verifier une fois composants souder!
             enc = getEncoderValue();
-            if(enc.hasChanged){
+            if (enc.hasChanged)
+            {
                 Serial.println(enc.direction);
-                if(enc.direction == 1){
-                    netIndex = (netIndex < networks.nets-1)?  netIndex + 1 : netIndex;
+                if (enc.direction == 1)
+                {
+                    netIndex = (netIndex < networks.nets - 1) ? netIndex + 1 : netIndex;
                     lcdClearLine(1);
                     lcdPrint(networks.SSID[netIndex]);
                 }
-                else if(enc.direction == -1){
-                    netIndex = (netIndex > 0)?  netIndex - 1 : netIndex;
+                else if (enc.direction == -1)
+                {
+                    netIndex = (netIndex > 0) ? netIndex - 1 : netIndex;
                     lcdClearLine(1);
                     lcdPrint(networks.SSID[netIndex]);
                 }
             }
         }
-        else{
-            if(checkBtnPress().time < 1000){
-                if(connectToNetwork(networks.SSID[netIndex],getPassword())){
+        else
+        {
+            if (checkBtnPress().time < 1000)
+            {
+                if (connectToNetwork(networks.SSID[netIndex], getPassword()))
+                {
                     //Connection réussie
                     lcdClear();
                     lcdPrint("Conn. réussie!");
@@ -39,7 +47,8 @@ void connectToWifi()
                     lcdClear();
                     break;
                 }
-                else{
+                else
+                {
                     //Connection échouée
                     lcdClear();
                     lcdPrint("Conn. échouée!");
@@ -49,17 +58,37 @@ void connectToWifi()
                     break;
                 }
             }
-            else{
+            else
+            {
                 //l'utilisateur souhaite rafraîchir la liste des PA.
                 networks = listNetworks();
             }
         }
-
     }
 }
 
-
 String getPassword()
 {
-    //TODO
+    char choices[] = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ _-?!,.:;%€$£@#*<>+=";
+    String pwd = "";
+    while (true)
+    {
+        if (!checkBtnPress().wasPressed)
+        {
+            //TODO move cursor pos according to encoder input
+        }
+        else
+        {
+            if (checkBtnPress().time < 1000)
+            {
+                //TODO get choice and add to pwd array
+                pwd += choices[0]; //TODO replace 0 by value
+            }
+            else
+            {
+                //TODO confirm and return pwd
+            }
+        }
+    }
+    return pwd;
 }
